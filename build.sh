b958249736f579c054361bd1ca32ba979600b0ae
Deshi Xiao <xiaods@users.noreply.github.com>
2016-07-26 14:05:09 +0800

    update image tag to catalog repository

--- build.sh
+++ build.sh
@@ -5,7 +5,9 @@ set -e
 # include haproxy binary
 IMAGE="omega-haproxyctl"
 VERSION="$(cat VERSION)"
+HOST="catalog.shurenyun.com"
+NAMESPACE="library"
 
 docker build --no-cache -t ${IMAGE}:${VERSION} -f Dockerfile.builder .
 
-docker tag ${IMAGE}:${VERSION} ${IMAGE}:latest
+docker tag ${IMAGE}:${VERSION} $(HOST)/$(NAMESPACE)/${IMAGE}:latest
